ab202fa3c08f4ab9588826049ed7708ec8bb10a3
ata4 <barracuda415@yahoo.de>
2019-08-31 14:17:49 +0200

    Improved readability

--- architecture.py
+++ architecture.py
@@ -21,10 +21,12 @@ class RRDB_Net(nn.Module):
             upsample_block = B.pixelshuffle_block
         else:
             raise NotImplementedError('upsample mode [%s] is not found' % upsample_mode)
+
         if upscale == 3:
             upsampler = upsample_block(nf, nf, 3, act_type=act_type)
         else:
             upsampler = [upsample_block(nf, nf, act_type=act_type) for _ in range(n_upscale)]
+
         HR_conv0 = B.conv_block(nf, nf, kernel_size=3, norm_type=None, act_type=act_type)
         HR_conv1 = B.conv_block(nf, out_nc, kernel_size=3, norm_type=None, act_type=None)
 
