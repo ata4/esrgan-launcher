ac2e6655d87316b274783cfc0d2ea7ed20778def
ata4 <barracuda415@yahoo.de>
2019-08-31 14:24:13 +0200

    Looks like a typo to me

--- architecture.py
+++ architecture.py
@@ -16,7 +16,7 @@ class RRDB_Net(nn.Module):
         LR_conv = B.conv_block(nf, nf, kernel_size=3, norm_type=norm_type, act_type=None, mode=mode)
 
         if upsample_mode == 'upconv':
-            upsample_block = B.upconv_blcok
+            upsample_block = B.upconv_block
         elif upsample_mode == 'pixelshuffle':
             upsample_block = B.pixelshuffle_block
         else:
--- block.py
+++ block.py
@@ -251,7 +251,7 @@ def pixelshuffle_block(in_nc, out_nc, upscale_factor=2, kernel_size=3, stride=1,
     return sequential(conv, pixel_shuffle, n, a)
 
 
-def upconv_blcok(in_nc, out_nc, upscale_factor=2, kernel_size=3, stride=1, bias=True,
+def upconv_block(in_nc, out_nc, upscale_factor=2, kernel_size=3, stride=1, bias=True,
                 pad_type='zero', norm_type=None, act_type='relu', mode='nearest'):
     # Up conv
     # described in https://distill.pub/2016/deconv-checkerboard/
