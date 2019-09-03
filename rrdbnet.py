a1adba072cce8cb33511a05cb3bdd0f916758df1
ata4 <barracuda415@yahoo.de>
2019-09-03 17:57:32 +0200

    Added support for old model format and scales other than 4x

--- rrdbnet.py
+++ rrdbnet.py
@@ -1,4 +1,5 @@
 import functools
+import collections
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,6 +11,61 @@ def make_layer(block, n_layers):
         layers.append(block())
     return nn.Sequential(*layers)
 
+def get_scale_index(state_dict):
+    # this is more or less guesswork, since I haven't seen any
+    # non-4x models using the new format in the wild, but it
+    # should work in theory
+    max_index = 0
+
+    for k in state_dict.keys():
+        if k.startswith("upconv") and k.endswith(".weight"):
+            max_index = max(max_index, int(k[6:-7]))
+
+    return max_index
+
+def get_legacy_scale_index(state_dict):
+    try:
+        # get largest model index from keys like "model.X.weight"
+        max_index = max([int(n.split(".")[1]) for n in state_dict.keys()])
+    except:
+        # invalid model dict format?
+        raise RuntimeError("Unable to determine scale index for model")
+
+    return (max_index - 4) // 3
+
+def build_legacy_keymap(n_upscale):
+    keymap = collections.OrderedDict()
+    keymap["model.0"] = "conv_first"
+
+    for i in range(23):
+        for j in range(1, 4):
+            for k in range(1, 6):
+                k1 = "model.1.sub.%d.RDB%d.conv%d.0" % (i, j, k)
+                k2 = "RRDB_trunk.%d.RDB%d.conv%d" % (i, j, k)
+                keymap[k1] = k2
+
+    keymap["model.1.sub.23"] = "trunk_conv"
+
+    n = 0
+    for i in range(1, n_upscale + 1):
+        n += 3
+        k1 = "model.%d" % n
+        k2 = "upconv%d" % i
+        keymap[k1] = k2
+
+    keymap["model.%d" % (n + 2)] = "HRconv"
+    keymap["model.%d" % (n + 4)] = "conv_last"
+
+    # add ".weigth" and ".bias" suffixes to all keys
+    keymap_final = collections.OrderedDict()
+
+    for k1, k2 in keymap.items():
+        for k_type in ("weight", "bias"):
+            k1_f = k1 + "." +  k_type
+            k2_f = k2 + "." +  k_type
+            keymap_final[k1_f] = k2_f
+
+    return keymap_final
 
 class ResidualDenseBlock_5C(nn.Module):
     def __init__(self, nf=64, gc=32, bias=True):
@@ -49,7 +105,6 @@ class RRDB(nn.Module):
         out = self.RDB3(out)
         return out * 0.2 + x
 
-
 class RRDBNet(nn.Module):
     def __init__(self, in_nc, out_nc, nf, nb, gc=32):
         super(RRDBNet, self).__init__()
@@ -59,20 +114,41 @@ class RRDBNet(nn.Module):
         self.RRDB_trunk = make_layer(RRDB_block_f, nb)
         self.trunk_conv = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
         #### upsampling
-        self.upconv1 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
-        self.upconv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
         self.HRconv = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
         self.conv_last = nn.Conv2d(nf, out_nc, 3, 1, 1, bias=True)
 
         self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+        self.n_upscale = 0
+        self.nf = nf
+
+    def load_state_dict(self, state_dict, strict=True):
+        # check for legacy model format
+        if "model.0.weight" in state_dict:
+            # remap dict keys to new format
+            self.n_upscale = get_legacy_scale_index(state_dict)
+            keymap = build_legacy_keymap(self.n_upscale)
+            state_dict = {keymap[k]: v for k, v in state_dict.items()}
+        else:
+            self.n_upscale = get_scale_index(state_dict)
+
+        # build upconv layers based on model scale
+        for n in range(1, self.n_upscale + 1):
+            upconv = nn.Conv2d(self.nf, self.nf, 3, 1, 1, bias=True)
+            setattr(self, "upconv%d" % n, upconv)
+
+        return super().load_state_dict(state_dict, strict)
 
     def forward(self, x):
         fea = self.conv_first(x)
         trunk = self.trunk_conv(self.RRDB_trunk(fea))
         fea = fea + trunk
 
-        fea = self.lrelu(self.upconv1(F.interpolate(fea, scale_factor=2, mode='nearest')))
-        fea = self.lrelu(self.upconv2(F.interpolate(fea, scale_factor=2, mode='nearest')))
+        # apply upconv layers
+        for n in range(1, self.n_upscale + 1):
+            upconv = getattr(self, "upconv%d" % n)
+            fea = self.lrelu(upconv(F.interpolate(fea, scale_factor=2, mode='nearest')))
+
         out = self.conv_last(self.lrelu(self.HRconv(fea)))
 
         return out
